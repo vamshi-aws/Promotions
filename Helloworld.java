e95357839b4af570b3dd7327c09da48e8bdf0df0
vamshi-aws <47096302+vamshi-aws@users.noreply.github.com>
2019-06-06 16:51:24 +0530

    Delete Helloworld.java

--- Helloworld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World, How are you, Fine");
-      System.out.println("My name is Vamshi Madishetty space check");
-   }
-}
